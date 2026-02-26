9329bce353bd4a142f2196ad7a4be41b7f9cc70e
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2026-02-26 15:50:13 +0700

    updatedata

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
