1d226d0cecf9c7d20f8b1835e46bc70201e9e638
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2026-02-26 15:26:09 +0700

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = \App\Models\User::where('email', $request->email)->first();
+
+        if (!$user || !\Illuminate\Support\Facades\Hash::check($request->password, $user->password)) {
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'email' => ['Kredensial yang diberikan salah.'],
+            ]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken('api-token')->plainTextToken,
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Berhasil logout'
+        ]);
+    }
+}
