e1e7fafc4c8e9fad191f7ec4484bc97a7ea8f688
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2026-03-05 13:37:52 +0700

    feat: add remote seeder endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/seed-db', function () {
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+    return response()->json([
+        'message' => 'Database seeded successfully!',
+        'output' => \Illuminate\Support\Facades\Artisan::output()
+    ]);
+});
