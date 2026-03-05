e7fcdb291a7645b245f7e5ed6f342954a2a2e737
Muhammad Luthfi Naldi <129099749+AtomNano@users.noreply.github.com>
2026-03-05 13:41:43 +0700

    chore: remove temporary seeder endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/seed-db', function () {
-    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
-    return response()->json([
-        'message' => 'Database seeded successfully!',
-        'output' => \Illuminate\Support\Facades\Artisan::output()
-    ]);
-});
